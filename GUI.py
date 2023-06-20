a44af73192e8b44c28d42a68aabaf43a48bbecb0
MrNemesis98 <84439444+MrNemesis98@users.noreply.github.com>
2023-06-20 22:43:23 +0200

    GUI Progress (Main Menus)

--- GUI.py
+++ GUI.py
@@ -28,6 +28,13 @@ class Initiator(QMainWindow):
         self.main_background_label = None
         self.menu_bar_label = None
 
+        self.mbl_alpha_button = None
+        self.mbl_beta_button = None
+        self.mbl_gamma_button = None
+        self.mbl_delta_button = None
+        self.mbl_epsilon_button = None
+        self.mbl_zeta_button = None
+
         self.timer = QTimer()
 
         self.create_window()
@@ -37,6 +44,7 @@ class Initiator(QMainWindow):
     closed = pyqtSignal()
 
     gui_mode = None
+    menu_buttons_restricted = False
     entries_history = None
 
     # get current status -----------------------------------------------------------------------------------------------
@@ -60,27 +68,45 @@ class Initiator(QMainWindow):
         # Labels -------------------------------------------------------------------------------------------------------
 
         self.menu_bar_label = QLabel(self)
-        self.menu_bar_label.setGeometry(0, 0, 120, 1000)
+        self.menu_bar_label.setGeometry(0, 0, 100, 1000)
         self.menu_bar_label.setStyleSheet(GSS.menu_bar_label())
         self.menu_bar_label.show()
 
         # Buttons ------------------------------------------------------------------------------------------------------
 
-        mbl_alpha_button = QPushButton(self)
-        mbl_alpha_button.setGeometry(10, 50, 100, 100)
-        mbl_alpha_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_alpha_button = QPushButton(self)
+        self.mbl_alpha_button.setGeometry(0, 40, 100, 100)
+        self.mbl_alpha_button.clicked.connect(self.mbl_alpha_button_pressed)
 
-        mbl_beta_button = QPushButton(self)
-        mbl_beta_button.setGeometry(10, 550, 100, 100)
-        mbl_beta_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_beta_button = QPushButton(self)
+        self.mbl_beta_button.setGeometry(0, 300, 100, 100)
+        self.mbl_beta_button.clicked.connect(self.mbl_beta_button_pressed)
 
-        mbl_gamma_button = QPushButton(self)
-        mbl_gamma_button.setGeometry(10, 700, 100, 100)
-        mbl_gamma_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_gamma_button = QPushButton(self)
+        self.mbl_gamma_button.setGeometry(0, 420, 100, 100)
+        self.mbl_gamma_button.clicked.connect(self.mbl_gamma_button_pressed)
 
-        mbl_delta_button = QPushButton(self)
-        mbl_delta_button.setGeometry(10, 850, 100, 100)
-        mbl_delta_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_delta_button = QPushButton(self)
+        self.mbl_delta_button.setGeometry(0, 540, 100, 100)
+        self.mbl_delta_button.clicked.connect(self.mbl_delta_button_pressed)
+
+        self.mbl_epsilon_button = QPushButton(self)
+        self.mbl_epsilon_button.setGeometry(0, 660, 100, 100)
+        self.mbl_epsilon_button.clicked.connect(self.mbl_epsilon_button_pressed)
+
+        self.mbl_zeta_button = QPushButton(self)
+        self.mbl_zeta_button.setGeometry(0, 880, 100, 100)
+        self.mbl_zeta_button.clicked.connect(self.mbl_zeta_button_pressed)
+
+        """
+        self.mbl_eta_button = QPushButton(self)
+        self.mbl_eta_button.setGeometry(0, 770, 100, 100)
+        self.mbl_eta_button.setStyleSheet(GSS.mbl_alpha_button())
+
+        self.mbl_theta_button = QPushButton(self)
+        self.mbl_theta_button.setGeometry(0, 885, 100, 100)
+        self.mbl_theta_button.setStyleSheet(GSS.mbl_alpha_button())
+        """
 
         # Special Items ------------------------------------------------------------------------------------------------
 
@@ -100,17 +126,89 @@ class Initiator(QMainWindow):
         self.checkbox.clicked.connect(self.checkbox_clicked)
         """
 
-        self.main_menu()
+        self.home_menu()
 
     #  MENUS ***********************************************************************************************************
 
-    def main_menu(self):
-        self.gui_mode = "main_menu"
+    def home_menu(self):
+        self.gui_mode = "home_menu"
+        self.mbl_alpha_button.setStyleSheet(GSS.mbl_alpha_button(selected=True))
+        self.mbl_beta_button.setStyleSheet(GSS.mbl_beta_button())
+        self.mbl_gamma_button.setStyleSheet(GSS.mbl_gamma_button())
+        self.mbl_delta_button.setStyleSheet(GSS.mbl_delta_button())
+        self.mbl_epsilon_button.setStyleSheet(GSS.mbl_epsilon_button())
+        self.mbl_zeta_button.setStyleSheet(GSS.mbl_zeta_button())
 
+    def database_menu(self):
+        self.gui_mode = "home_menu"
+        self.mbl_alpha_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_beta_button.setStyleSheet(GSS.mbl_beta_button(selected=True))
+        self.mbl_gamma_button.setStyleSheet(GSS.mbl_gamma_button())
+        self.mbl_delta_button.setStyleSheet(GSS.mbl_delta_button())
+        self.mbl_epsilon_button.setStyleSheet(GSS.mbl_epsilon_button())
+        self.mbl_zeta_button.setStyleSheet(GSS.mbl_zeta_button())
 
+    def search_menu(self):
+        self.gui_mode = "home_menu"
+        self.mbl_alpha_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_beta_button.setStyleSheet(GSS.mbl_beta_button())
+        self.mbl_gamma_button.setStyleSheet(GSS.mbl_gamma_button(selected=True))
+        self.mbl_delta_button.setStyleSheet(GSS.mbl_delta_button())
+        self.mbl_epsilon_button.setStyleSheet(GSS.mbl_epsilon_button())
+        self.mbl_zeta_button.setStyleSheet(GSS.mbl_zeta_button())
 
+    def scan_menu(self):
+        self.gui_mode = "home_menu"
+        self.mbl_alpha_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_beta_button.setStyleSheet(GSS.mbl_beta_button())
+        self.mbl_gamma_button.setStyleSheet(GSS.mbl_gamma_button())
+        self.mbl_delta_button.setStyleSheet(GSS.mbl_delta_button(selected=True))
+        self.mbl_epsilon_button.setStyleSheet(GSS.mbl_epsilon_button())
+        self.mbl_zeta_button.setStyleSheet(GSS.mbl_zeta_button())
+
+    def output_menu(self):
+        self.gui_mode = "home_menu"
+        self.mbl_alpha_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_beta_button.setStyleSheet(GSS.mbl_beta_button())
+        self.mbl_gamma_button.setStyleSheet(GSS.mbl_gamma_button())
+        self.mbl_delta_button.setStyleSheet(GSS.mbl_delta_button())
+        self.mbl_epsilon_button.setStyleSheet(GSS.mbl_epsilon_button(selected=True))
+        self.mbl_zeta_button.setStyleSheet(GSS.mbl_zeta_button())
+
+    def settings_menu(self):
+        self.gui_mode = "home_menu"
+        self.mbl_alpha_button.setStyleSheet(GSS.mbl_alpha_button())
+        self.mbl_beta_button.setStyleSheet(GSS.mbl_beta_button())
+        self.mbl_gamma_button.setStyleSheet(GSS.mbl_gamma_button())
+        self.mbl_delta_button.setStyleSheet(GSS.mbl_delta_button())
+        self.mbl_epsilon_button.setStyleSheet(GSS.mbl_epsilon_button())
+        self.mbl_zeta_button.setStyleSheet(GSS.mbl_zeta_button(selected=True))
 
     # Button Functions
+    def mbl_alpha_button_pressed(self):
+        if not self.menu_buttons_restricted:
+            self.home_menu()
+
+    def mbl_beta_button_pressed(self):
+        if not self.menu_buttons_restricted:
+            self.database_menu()
+
+    def mbl_gamma_button_pressed(self):
+        if not self.menu_buttons_restricted:
+            self.search_menu()
+
+    def mbl_delta_button_pressed(self):
+        if not self.menu_buttons_restricted:
+            self.scan_menu()
+
+    def mbl_epsilon_button_pressed(self):
+        if not self.menu_buttons_restricted:
+            self.output_menu()
+
+    def mbl_zeta_button_pressed(self):
+        if not self.menu_buttons_restricted:
+            self.settings_menu()
+
 
 
 
--- GUI_Stylesheets.py
+++ GUI_Stylesheets.py
@@ -6,27 +6,71 @@ def main_background_label():
 def menu_bar_label():
     stylesheet = "background: qlineargradient" \
                  "(x1: 0, y1: 0, x2: 0, y2: 1, " \
-                 "stop: 0 rgba(60, 60, 60, 255), stop: 1 rgba(55, 55, 55, 255));"
+                 "stop: 0 rgba(50, 50, 50, 255), stop: 1 rgba(40, 40, 40, 255));"
     return stylesheet
 
 
 # buttons **************************************************************************************************************
 
-def image_button_stylesheet(standard_link, hover_link, pressed_link):
-    stylesheet = "QPushButton {" \
-                 "background-image: url(" + standard_link + ");" \
-                 "background-repeat: no-repeat;" \
-                 "background-position: center;" \
-                 "background-color: transparent;""}" \
-                 "QPushButton:hover {" \
-                 "background-image: url(" + hover_link + ");""}" \
-                 "QPushButton:pressed {" \
-                 "background-image: url(" + pressed_link + ");""}"
+def image_button_stylesheet(standard_link, hover_link, pressed_link, selected=False):
+    if selected:
+        stylesheet = "QPushButton {" \
+                     "border-radius: 40pt;" \
+                     "background-image: url(" + pressed_link + ");" \
+                     "background-repeat: no-repeat;" \
+                     "background-position: center;" \
+                     "background-color: transparent;}"
+    else:
+        stylesheet = "QPushButton {" \
+         "border-radius: 40pt;"\
+         "background-image: url(" + standard_link + ");" \
+         "background-repeat: no-repeat;" \
+         "background-position: center;" \
+         "background-color: transparent;}" \
+         "QPushButton:hover {" \
+         "background-image: url(" + hover_link + ");}" \
+         "QPushButton:pressed {" \
+         "background-image: url(" + pressed_link + ");}"
     return stylesheet
 
 
-def mbl_alpha_button():
-    standard_link = "data/GUI_img/check_S.png"
-    hover_link = "data/GUI_img/check_M.png"
-    pressed_link = "data/GUI_img/check_L.png"
-    return image_button_stylesheet(standard_link, hover_link, pressed_link)
+def mbl_alpha_button(selected=False):
+    standard_link = "data/GUI_img/Home_Std.png"
+    hover_link = "data/GUI_img/Home_Sel.png"
+    pressed_link = "data/GUI_img/Home_Sel.png"
+    return image_button_stylesheet(standard_link, hover_link, pressed_link, selected)
+
+
+def mbl_beta_button(selected=False):
+    standard_link = "data/GUI_img/Database_Std.png"
+    hover_link = "data/GUI_img/Database_Sel.png"
+    pressed_link = "data/GUI_img/Database_Sel.png"
+    return image_button_stylesheet(standard_link, hover_link, pressed_link, selected)
+
+
+def mbl_gamma_button(selected=False):
+    standard_link = "data/GUI_img/Search_Std.png"
+    hover_link = "data/GUI_img/Search_Sel.png"
+    pressed_link = "data/GUI_img/Search_Sel.png"
+    return image_button_stylesheet(standard_link, hover_link, pressed_link, selected)
+
+
+def mbl_delta_button(selected=False):
+    standard_link = "data/GUI_img/Scan_Std.png"
+    hover_link = "data/GUI_img/Scan_Sel.png"
+    pressed_link = "data/GUI_img/Scan_Sel.png"
+    return image_button_stylesheet(standard_link, hover_link, pressed_link, selected)
+
+
+def mbl_epsilon_button(selected=False):
+    standard_link = "data/GUI_img/Output_Std.png"
+    hover_link = "data/GUI_img/Output_Sel.png"
+    pressed_link = "data/GUI_img/Output_Sel.png"
+    return image_button_stylesheet(standard_link, hover_link, pressed_link, selected)
+
+
+def mbl_zeta_button(selected=False):
+    standard_link = "data/GUI_img/Settings_Std.png"
+    hover_link = "data/GUI_img/Settings_Sel.png"
+    pressed_link = "data/GUI_img/Settings_Sel.png"
+    return image_button_stylesheet(standard_link, hover_link, pressed_link, selected)
